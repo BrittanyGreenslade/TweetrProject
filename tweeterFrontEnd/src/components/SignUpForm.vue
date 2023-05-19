8279497eba7190949c2a4a7e01d213382ab9c620
Brittany Greenslade <bgreensl@ualberta.ca>
2023-05-19 14:29:47 -0600

    url

--- tweeterFrontEnd/src/components/SignUpForm.vue
+++ tweeterFrontEnd/src/components/SignUpForm.vue
@@ -57,7 +57,7 @@ export default {
     userSignup() {
       axios
         .request({
-          url: `${process.env.VUE_APP_API_URL}/users`,
+          url: `api/users`,
           method: "POST",
           headers: {
             "Content-Type": "application/json",
